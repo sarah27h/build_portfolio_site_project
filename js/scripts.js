4c44e9eaf2bdb01757f070101ad8c075bb340758
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-11-21 20:59:46 +0200

    Style: add new project, change projects order

--- js/scripts.js
+++ js/scripts.js
@@ -26,41 +26,48 @@ let projects = [
         git: 'https://github.com/sarah27h/FEND-restaurant-stage-1'
     },
     {
-        title: 'Memory game',
+        title: 'Get Weather React',
         src: 'images/project4.png',
+        alt: 'Get Weather project',
+        href: 'https://sarah27h.github.io/react-get-weather-app/',
+        git: 'https://github.com/sarah27h/react-get-weather-app'
+    },
+    {
+        title: 'Memory game',
+        src: 'images/project11.png',
         alt: 'Memory game project',
         href: 'https://sarah27h.github.io/fend-project-memory-game-master/',
         git: 'https://github.com/sarah27h/fend-project-memory-game-master'
     },
     {
         title: 'Arcade game',
-        src: 'images/project5.png',
+        src: 'images/project12.png',
         alt: 'Arcade game project',
         href: 'https://sarah27h.github.io/arcade-game-master/',
         git: 'https://github.com/sarah27h/arcade-game-master'
     },
     {
         title: 'feed reader testing',
-        src: 'images/project6.png',
+        src: 'images/project13.png',
         alt: 'feed reader testing project',
         href: 'https://sarah27h.github.io/feed-reader/',
         git: 'https://github.com/sarah27h/feed-reader'
     },
     {
         title: 'bakery website',
-        src: 'images/project7.png',
+        src: 'images/project14.png',
         alt: 'best bakery website project',
         href: 'http://bestbakery.000webhostapp.com/'
     },
     {
         title: 'Pixel Art Maker',
-        src: 'images/project8.png',
+        src: 'images/project15.png',
         alt: 'pixel art project',
         href: 'https://codepen.io/Sarah27h/full/QagrVa/'
     },
     {
         title: 'Animal Card',
-        src: 'images/project9.png',
+        src: 'images/project16.png',
         alt: 'panda animal',
         href: 'https://codepen.io/Sarah27h/pen/RjLwKR?editors=1000'
     }
