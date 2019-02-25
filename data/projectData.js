c3f49eeee8d4958d6b6186a98d0de7052b61d533
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-02-25 20:46:21 +0200

    Feat: move projects data in seperate file

---
+++ data/projectData.js
@@ -0,0 +1,83 @@
+/*
+ *
+ * data
+ *
+*/
+
+const  PROJRECTS = [
+    {
+        title: 'museums guide react',
+        src: 'images/project1.png',
+        alt: 'Museum guide project',
+        href: 'https://sarah27h.github.io/test-deploy/',
+        git: 'https://github.com/sarah27h/FEND-neighborhood-map-react'
+    },
+    {
+        title: 'myreads react',
+        src: 'images/project2.png',
+        alt: 'myreads react project',
+        href: 'https://sarah27h.github.io/reactnd-project-myreads-deploy/#/reactnd-project-myreads-deploy/',
+        git: 'https://github.com/sarah27h/reactnd-project-myreads-starter'
+    },
+    {
+        title: 'project manager redux',
+        src: 'images/project3.png',
+        alt: 'project manager project',
+        href: 'https://project-manager-7b746.firebaseapp.com/',
+        git: 'https://github.com/sarah27h/projects-manager'
+    },
+    {
+        title: 'resturant reviews',
+        src: 'images/project4.png',
+        alt: 'resturant reviews project',
+        href: 'https://sarah27h.github.io/FEND-restaurant-stage-1/',
+        git: 'https://github.com/sarah27h/FEND-restaurant-stage-1'
+    },
+    {
+        title: 'Get Weather React',
+        src: 'images/project6.png',
+        alt: 'Get Weather project',
+        href: 'https://sarah27h.github.io/react-get-weather-app/',
+        git: 'https://github.com/sarah27h/react-get-weather-app'
+    },
+    {
+        title: 'Memory game',
+        src: 'images/project11.png',
+        alt: 'Memory game project',
+        href: 'https://sarah27h.github.io/fend-project-memory-game-master/',
+        git: 'https://github.com/sarah27h/fend-project-memory-game-master'
+    },
+    {
+        title: 'Arcade game',
+        src: 'images/project12.png',
+        alt: 'Arcade game project',
+        href: 'https://sarah27h.github.io/arcade-game-master/',
+        git: 'https://github.com/sarah27h/arcade-game-master'
+    },
+    {
+        title: 'feed reader testing',
+        src: 'images/project13.png',
+        alt: 'feed reader testing project',
+        href: 'https://sarah27h.github.io/feed-reader/',
+        git: 'https://github.com/sarah27h/feed-reader'
+    },
+    {
+        title: 'bakery website',
+        src: 'images/project14.png',
+        alt: 'best bakery website project',
+        href: 'http://bestbakery.000webhostapp.com/'
+    },
+    {
+        title: 'Pixel Art Maker',
+        src: 'images/project15.png',
+        alt: 'pixel art project',
+        href: 'https://codepen.io/Sarah27h/full/QagrVa/'
+    },
+    {
+        title: 'Animal Card',
+        src: 'images/project16.png',
+        alt: 'panda animal',
+        href: 'https://codepen.io/Sarah27h/pen/RjLwKR?editors=1000'
+    }
+];
+
