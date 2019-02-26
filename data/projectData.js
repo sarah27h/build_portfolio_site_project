b95c36694247007f914b8ebd9a7a50c9ddbbf2cd
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-02-26 21:07:20 +0200

    Style: add projects info and techs used

--- data/projectData.js
+++ data/projectData.js
@@ -10,68 +10,88 @@ const  PROJRECTS = [
         src: 'images/project1.png',
         alt: 'Museum guide project',
         href: 'https://sarah27h.github.io/test-deploy/',
-        git: 'https://github.com/sarah27h/FEND-neighborhood-map-react'
+        git: 'https://github.com/sarah27h/FEND-neighborhood-map-react',
+        info: 'The project requirements are to develop a single page application featuring a map of your neighborhood. Then add functionality to this map including highlighted locations, third-party data about those locations and various ways to browse the content. Succeeded in getting a score of 100% for accessibility and 88% for best practices after performing Audits test.',
+        tech: 'React.js, Google Maps API, FourSquare API, AJAX, Responsive and Accessible'
     },
     {
         title: 'myreads react',
         src: 'images/project2.png',
         alt: 'myreads react project',
         href: 'https://sarah27h.github.io/reactnd-project-myreads-deploy/#/reactnd-project-myreads-deploy/',
-        git: 'https://github.com/sarah27h/reactnd-project-myreads-starter'
+        git: 'https://github.com/sarah27h/reactnd-project-myreads-starter',
+        info: 'The project requirements are, create a bookshelf app that allows a user to select and categorize books they have read, are currently reading, or want to read.',
+        tech: 'React.js, CSS, HTML, Javascript and Cross-Browser-Testing'
     },
     {
         title: 'project manager redux',
         src: 'images/project3.png',
         alt: 'project manager project',
         href: 'https://project-manager-7b746.firebaseapp.com/',
-        git: 'https://github.com/sarah27h/projects-manager'
+        git: 'https://github.com/sarah27h/projects-manager',
+        info: 'The project is a projects manager users can sign up and create their own projects store them on the firebase.',
+        tech: 'React.js, Redux, Thunk, Firebase, Materialize CSS'
     },
     {
         title: 'resturant reviews',
         src: 'images/project4.png',
         alt: 'resturant reviews project',
         href: 'https://sarah27h.github.io/FEND-restaurant-stage-1/',
-        git: 'https://github.com/sarah27h/FEND-restaurant-stage-1'
+        git: 'https://github.com/sarah27h/FEND-restaurant-stage-1',
+        info: 'The project requirements are to convert a static webpage to a mobile-ready web application. Webpage needed to take a static design that lacks accessibility and converts the design to be responsive on different sized displays and accessible for screen reader users. Succeeded in getting a score of 94% for accessibility and best practices after performing Audits test.',
+        tech: 'Javascript, HTML, CSS, Mapbox, Responsive, Accessible, '
     },
     {
         title: 'Get Weather React',
         src: 'images/project6.png',
         alt: 'Get Weather project',
         href: 'https://sarah27h.github.io/react-get-weather-app/',
-        git: 'https://github.com/sarah27h/react-get-weather-app'
+        git: 'https://github.com/sarah27h/react-get-weather-app',
+        info: 'This project is a single page application from scratch to work as an interactive weather app',
+        tech: 'React.js, Responsive, Accessible, Cross-browsers, openweathermap API and AJAX '
     },
     {
         title: 'Memory game',
         src: 'images/project11.png',
         alt: 'Memory game project',
         href: 'https://sarah27h.github.io/fend-project-memory-game-master/',
-        git: 'https://github.com/sarah27h/fend-project-memory-game-master'
+        git: 'https://github.com/sarah27h/fend-project-memory-game-master',
+        info: 'The project requirements are to build a complete browser-based card matching game. This game is well-designed, feature-packed memory game, with timer, modal box, score, and responsive in all screen sizes. Succeeded in getting a score of 83% for accessibility and 88% for best practices after performing Audits test.',
+        tech: 'Javascript, CSS3, HTML and Responsive'
     },
     {
         title: 'Arcade game',
         src: 'images/project12.png',
         alt: 'Arcade game project',
         href: 'https://sarah27h.github.io/arcade-game-master/',
-        git: 'https://github.com/sarah27h/arcade-game-master'
+        git: 'https://github.com/sarah27h/arcade-game-master',
+        info: 'The project is provided visual assets and a game loop engine; using these tools I add a number of entities to the game including the player characters and enemies to recreate the classic arcade game Frogger. Succeeded in getting a score of 90% for accessibility, 88% for best practices, and 75% for performance after performing Audits test.',
+        tech: 'Javascript, Object-Oriented Programming and HTML5 Canvas'
     },
     {
         title: 'feed reader testing',
         src: 'images/project13.png',
         alt: 'feed reader testing project',
         href: 'https://sarah27h.github.io/feed-reader/',
-        git: 'https://github.com/sarah27h/feed-reader'
+        git: 'https://github.com/sarah27h/feed-reader',
+        info: 'The project is part of the FEND Nanodegree, given a web-based application that reads RSS feeds.',
+        tech: 'Jasmine 2.2 unit-testing'
     },
     {
         title: 'bakery website',
         src: 'images/project14.png',
         alt: 'best bakery website project',
-        href: 'http://bestbakery.000webhostapp.com/'
+        href: 'http://bestbakery.000webhostapp.com/',
+        info: 'Best Bakery is a beautiful and unique one page HTML template. Created to fit your small business like cake shops, patisserie shops, and bakeries.',
+        tech: 'Bootstrap, Jquery, Javascript, Google maps, HTML, CSS, Responsive and Handlebars library'
     },
     {
         title: 'Pixel Art Maker',
         src: 'images/project15.png',
         alt: 'pixel art project',
-        href: 'https://codepen.io/Sarah27h/full/QagrVa/'
+        href: 'https://codepen.io/Sarah27h/full/QagrVa/',
+        info: 'Build a single-page web app using HTML, CSS, and JQuery convert starter code, including HTML and CSS to an interactive app, allow the user to create a grid of squares representing their design.',
+        tech: 'Jquery, HTML, CSS'
     }
     // {
     //     title: 'Animal Card',
