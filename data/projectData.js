b2bcef505316d4744a7d79481289ed7e8b863864
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2019-04-22 20:16:41 +0200

    Style: update projects data

--- data/projectData.js
+++ data/projectData.js
@@ -6,12 +6,21 @@
 
 const  PROJRECTS = [
     {
+        title: 'project manager redux',
+        src: 'images/project3.png',
+        alt: 'project manager project',
+        href: 'https://project-manager-7b746.firebaseapp.com/',
+        git: 'https://github.com/sarah27h/projects-manager',
+        info: '● Built a simple project management tool with authentication and cloud functions. Users can sign up, create their own projects and store them on the firebase. ●  Succeeded in getting a score of 92% for accessibility, 86% for best practices, and 68% for performance after performing Audits test.',
+        tech: 'React.js, Redux, Thunk, Firebase, Materialize CSS'
+    },
+    {
         title: 'museums guide react',
         src: 'images/project1.png',
         alt: 'Museum guide project',
         href: 'https://sarah27h.github.io/test-deploy/',
         git: 'https://github.com/sarah27h/FEND-neighborhood-map-react',
-        info: 'The project requirements are to develop a single page application featuring a map of your neighborhood. Then add functionality to this map including highlighted locations, third-party data about those locations and various ways to browse the content. Succeeded in getting a score of 100% for accessibility and 88% for best practices after performing Audits test.',
+        info: '● Built user-friendly interface, responsive single page app and cross-browser using React, JS, JSX, HTML and CSS bringing site from paper prototype to work as an interactive museums guide. ● Fetched addresses from 2 APIs Google Maps and FourSquare using AJAX for more than 10 museums. ● Built a filter to search in museums list and had an error handling messages. ● Succeeded in getting a score of 100% for accessibility and 88% for best practices after performing Audits test.',
         tech: 'React.js, Google Maps API, FourSquare API, AJAX, Responsive, Accessible'
     },
     {
@@ -20,17 +29,8 @@ const  PROJRECTS = [
         alt: 'myreads react project',
         href: 'https://sarah27h.github.io/reactnd-project-myreads-deploy/#/reactnd-project-myreads-deploy/',
         git: 'https://github.com/sarah27h/reactnd-project-myreads-starter',
-        info: 'The project requirements are, create a bookshelf app that allows a user to select and categorize books they have read, are currently reading, or want to read.',
+        info: '● Maintain a static book lending app using React, JS, JSX, HTML, and CSS to an interactive, cross-browser app that enables users to have their own library (MyReads) and move books within 3 shelves. ● Built a search filter using React.js to allow users to search for more books in the main library, fetched 20 books from the database to select and add them to their own library.',
         tech: 'React.js, CSS, HTML, Javascript, Cross-Browser-Testing'
-    },
-    {
-        title: 'project manager redux',
-        src: 'images/project3.png',
-        alt: 'project manager project',
-        href: 'https://project-manager-7b746.firebaseapp.com/',
-        git: 'https://github.com/sarah27h/projects-manager',
-        info: 'The project is a projects manager users can sign up and create their own projects store them on the firebase.',
-        tech: 'React.js, Redux, Thunk, Firebase, Materialize CSS'
     },
     {
         title: 'resturant reviews',
@@ -38,7 +38,7 @@ const  PROJRECTS = [
         alt: 'resturant reviews project',
         href: 'https://sarah27h.github.io/FEND-restaurant-stage-1/',
         git: 'https://github.com/sarah27h/FEND-restaurant-stage-1',
-        info: 'The project requirements are to convert a static webpage to a mobile-ready web application. Webpage needed to take a static design that lacks accessibility and converts the design to be responsive on different sized displays and accessible for screen reader users. Succeeded in getting a score of 94% for accessibility and best practices after performing Audits test.',
+        info: '● Maintain a static design using HTML, CSS, JS, WCAG guidelines, Web Aim Checklist and Service Worker to be a mobile-ready web application,100% responsive for Web & Mobile, accessible and work offline and have responsive images for every viewport using srcset. ● Succeeded in getting a score of 94% for accessibility and best practices after performing Audits test.',
         tech: 'Javascript, HTML, CSS, Mapbox, Responsive, Accessible, '
     },
     {
@@ -47,7 +47,7 @@ const  PROJRECTS = [
         alt: 'Get Weather project',
         href: 'https://sarah27h.github.io/react-get-weather-app/',
         git: 'https://github.com/sarah27h/react-get-weather-app',
-        info: 'This project is a single page application from scratch to work as an interactive weather app',
+        info: '● Built a single page application from scratch to work as an interactive, responsive weather app. ● Succeeded in getting a score of 100% for accessibility, 93% for best practices and 71% for performance after performing Audits test.',
         tech: 'React.js, Responsive, Accessible, Cross-browsers, openweathermap API, AJAX '
     },
     {
@@ -56,7 +56,7 @@ const  PROJRECTS = [
         alt: 'Memory game project',
         href: 'https://sarah27h.github.io/fend-project-memory-game-master/',
         git: 'https://github.com/sarah27h/fend-project-memory-game-master',
-        info: 'The project requirements are to build a complete browser-based card matching game. This game is well-designed, feature-packed memory game, with timer, modal box, score, and responsive in all screen sizes. Succeeded in getting a score of 83% for accessibility and 88% for best practices after performing Audits test.',
+        info: '● Programmed a static HTML & CSS starter project using JS, HTML, CSS to make 100% an interactive and responsive game with a timer, modal box showing game score. ● Succeeded in getting a score of 83% for accessibility and 88% for best practices after performing Audits test.',
         tech: 'Javascript, CSS3, HTML, Responsive'
     },
     {
@@ -65,7 +65,7 @@ const  PROJRECTS = [
         alt: 'Arcade game project',
         href: 'https://sarah27h.github.io/arcade-game-master/',
         git: 'https://github.com/sarah27h/arcade-game-master',
-        info: 'The project is provided visual assets and a game loop engine; using these tools I add a number of entities to the game including the player characters and enemies to recreate the classic arcade game Frogger. Succeeded in getting a score of 90% for accessibility, 88% for best practices, and 75% for performance after performing Audits test.',
+        info: '● Programmed a static HTML & CSS starter project using Object-oriented JS, HTML5, and CSS to make an interactive game with a modal box showing the game score to improve the user experience. ● Added number of entities to the game including the player characters and enemies to recreate the classic arcade game Frogger.  ●  Succeeded in getting a score of 90% for accessibility, 88% for best practices, and 75% for performance after performing Audits test.',
         tech: 'Javascript, Object-Oriented Programming, HTML5 Canvas'
     },
     {
@@ -74,7 +74,7 @@ const  PROJRECTS = [
         alt: 'feed reader testing project',
         href: 'https://sarah27h.github.io/feed-reader/',
         git: 'https://github.com/sarah27h/feed-reader',
-        info: 'The project is part of the FEND Nanodegree, given a web-based application that reads RSS feeds.',
+        info: '● Used Jasmine 2.2 to test code against a pre-existing reads RSS feeds application to quickly test whether new code breaks an existing feature within my code, without having to manually test all of the functionality.',
         tech: 'Jasmine 2.2 unit-testing'
     },
     {
@@ -82,7 +82,7 @@ const  PROJRECTS = [
         src: 'images/project14.png',
         alt: 'best bakery website project',
         href: 'http://bestbakery.000webhostapp.com/',
-        info: 'Best Bakery is a beautiful and unique one page HTML template. Created to fit your small business like cake shops, patisserie shops, and bakeries.',
+        info: '● Built from scratch a responsive  HTML template. Created to fit a small business like cake shops, patisserie shops, and bakeries. ● Succeeded in getting a score of 75% for accessibility, 65% for both best practices and performance after performing Audits test.',
         tech: 'Bootstrap, Jquery, Javascript, Google maps, HTML, CSS, Responsive, Handlebars library'
     },
     {
@@ -90,7 +90,7 @@ const  PROJRECTS = [
         src: 'images/project15.png',
         alt: 'pixel art project',
         href: 'https://codepen.io/Sarah27h/full/QagrVa/',
-        info: 'Build a single-page web app using HTML, CSS, and JQuery convert starter code, including HTML and CSS to an interactive app, allow the user to create a grid of squares representing their design.',
+        info: '● Built an interactive app using HTML, CSS, and JQuery, allow the user to create a grid of colored squares representing their design.',
         tech: 'Jquery, HTML, CSS'
     }
     // {
